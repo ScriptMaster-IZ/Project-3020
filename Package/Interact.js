763a1db67d445e718cdc55c7d4598e7e8f3a8bdc
ScriptMaster-IZ <akukweizuchukwu@gmail.com>
2026-08-01 23:01:14 -0400

    Update Interact.js

--- Package/Interact.js
+++ Package/Interact.js
@@ -1,1 +1,33 @@
 
+function buttonCreate(text, link){
+//Finalize Link
+let hyperlink=document.createElement('a');
+hyperlink.setAttribute("href",link);
+
+
+
+//Finalize Button
+let butt=document.createElement('button');
+butt.innerText=text;
+
+//Add Button to hyeprlink
+hyperlink.appendChild(butt);
+
+//Find the holder element in teh page and 
+let hh=document.querySelector(".holder");
+
+//add the link button
+hh.appendChild(hyperlink);
+
+}
+
+
+
+
+(function buttonStart(){
+
+let butt1=new buttonCreate("HOME","About.html");
+butt1=new buttonCreate("ABOUT US","Services.html");
+
+}())
+
