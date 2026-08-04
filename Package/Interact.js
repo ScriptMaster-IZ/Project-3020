a499518d7e48e809f6e2e0e6f9df165f5d5146bd
ScriptMaster-IZ <akukweizuchukwu@gmail.com>
2026-08-04 03:30:04 -0400

    Update Interact.js

--- Package/Interact.js
+++ Package/Interact.js
@@ -1,13 +1,13 @@
 
 function buttonCreate(text, link){
+
 //Finalize Link
 let hyperlink=document.createElement('a');
 hyperlink.setAttribute("href",link);
 
-
-
 //Finalize Button
 let butt=document.createElement('button');
+butt.setAttribute("class","Pages");
 butt.innerText=text;
 
 //Add Button to hyeprlink
@@ -21,13 +21,42 @@ hh.appendChild(hyperlink);
 
 }
 
+function buttonTheme(text, link){
+
+//Finalize Button
+let butt=document.createElement('button');
+butt.setAttribute("id","theme-toggle");
+butt.setAttribute("class","Pages");
+
+butt.innerText=text;
+
+//Find the holder element in teh page and 
+let hh=document.querySelector(".holder");
+
+//add the link button
+hh.appendChild(butt);
+
+}
 
 
 
+
+
+
+
+//Navtigation buttons
 (function buttonStart(){
 
-let butt1=new buttonCreate("HOME","About.html");
-butt1=new buttonCreate("ABOUT US","Services.html");
+let butt1=new buttonCreate("HOME","Landing.html");
+butt1=new buttonCreate("ABOUT US","About.html");
+butt1=new buttonCreate("SERVICES","Services.html");
+butt1=new buttonCreate("PROJECTS/PRODUCTS","Project_Products.html");
+butt1=new buttonCreate("NEWS","Blog.html");
+butt1=new buttonCreate("CONTACTS","Contacts.html");
+
+butt1=new buttonTheme("THEME","Contacts.html");
+
+document.addEventListener("DOMContentLoaded", themer );
 
 }())
 
